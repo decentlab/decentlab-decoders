c05672866d684238614bfb31ba302ac7bd1ffa98
Khash Jalsan <khash.jalsan@decentlab.com>
2019-07-02 17:27:06 +0200

    DL-SHT3x: add (pub)

---
+++ DL-SHT3x/DL-SHT3x.ELEMENT-IoT.ex
@@ -0,0 +1,50 @@
+
+# https://www.decentlab.com/products/air-temperature-and-humidity-sensor-with-radiation-shield-for-lorawan
+
+defmodule Parser do
+  use Platform.Parsing.Behaviour
+  
+  ## test payloads
+  # 02030e000364a079b10c60
+  # 02030e00020c60
+  
+  def fields do
+    [
+      %{field: "air_temperature", display: "Air temperature", unit: "°C"},
+      %{field: "air_humidity", display: "Air humidity", unit: "%"},
+      %{field: "battery_voltage", display: "Battery voltage", unit: "V"}
+    ]
+  end
+
+  def parse(<<2, device_id::size(16), flags::binary-size(2), words::binary>>, _meta) do
+    {_remaining, result} =
+      {words, %{:device_id => device_id, :protocol_version => 2}}
+      |> sensor0(flags)
+      |> sensor1(flags)
+
+    result
+  end
+
+  
+  defp sensor0({<<x0::size(16), x1::size(16), remaining::binary>>, result},
+               <<_::size(15), 1::size(1), _::size(0)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :air_temperature => 175 * x0 / 65535 - 45,
+                 :air_humidity => 100 * x1 / 65535
+               })}
+  end
+  defp sensor0(result, _flags), do: result
+  
+  defp sensor1({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(14), 1::size(1), _::size(1)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :battery_voltage => x0 / 1000
+               })}
+  end
+  defp sensor1(result, _flags), do: result
+  
+end
---
+++ DL-SHT3x/DL-SHT3x.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# https://www.decentlab.com/products/air-temperature-and-humidity-sensor-with-radiation-shield-for-lorawan
+
+import struct
+from base64 import binascii
+
+PROTOCOL_VERSION = 2
+
+SENSORS = [
+    {'length': 2,
+     'values': [{'name': 'Air temperature',
+                 'convert': lambda x: 175 * x[0] / 65535 - 45,
+                 'unit': '°C'},
+                {'name': 'Air humidity',
+                 'convert': lambda x: 100 * x[1] / 65535,
+                 'unit': '%'}]},
+    {'length': 1,
+     'values': [{'name': 'Battery voltage',
+                 'convert': lambda x: x[0] / 1000,
+                 'unit': 'V'}]}
+]
+
+
+def decode(msg):
+    """msg: payload as one of hex string, list, or bytearray"""
+    bytes_ = bytearray(binascii.a2b_hex(msg)
+                       if isinstance(msg, str)
+                       else msg)
+    version = bytes_[0]
+    if version != PROTOCOL_VERSION:
+        raise ValueError("protocol version {} doesn't match v2".format(version))
+
+    devid = struct.unpack('>H', bytes_[1:3])[0]
+    bin_flags = bin(struct.unpack('>H', bytes_[3:5])[0])
+    flags = bin_flags[2:].zfill(struct.calcsize('>H') * 8)[::-1]
+
+    words = [struct.unpack('>H', bytes_[i:i + 2])[0]
+             for i
+             in range(5, len(bytes_), 2)]
+
+    cur = 0
+    result = {'Device ID': devid, 'Protocol version': version}
+    for flag, sensor in zip(flags, SENSORS):
+        if flag != '1':
+            continue
+
+        x = words[cur:cur + sensor["length"]]
+        cur += sensor["length"]
+        for value in sensor['values']:
+            if 'convert' not in value:
+                continue
+
+            result[value['name']] = {'value': value['convert'](x),
+                                     'unit': value.get('unit', None)}
+
+    return result
+
+
+if __name__ == '__main__':
+
+    import pprint
+    payloads = [
+        '02030e000364a079b10c60',
+        '02030e00020c60',
+    ]
+    for pl in payloads:
+        pprint.pprint(decode(pl))
+        print("")
