09d2f69fa5b85bc60c5d326f0fb36bff903ac0b5
Khash Jalsan <khash.jalsan@decentlab.com>
2020-02-05 18:37:42 +0100

    JavaScript: do not shift array, fixes #2 (pub)

--- DL-IAM/DL-IAM.js
+++ DL-IAM/DL-IAM.js
@@ -44,8 +44,8 @@ var decentlab_decoder = {
                unit: 'ppb'}]}
   ],
 
-  read_int: function (bytes) {
-    return (bytes.shift() << 8) + bytes.shift();
+  read_int: function (bytes, pos) {
+    return (bytes[pos] << 8) + bytes[pos + 1];
   },
 
   decode: function (msg) {
@@ -58,15 +58,16 @@ var decentlab_decoder = {
       }
     }
 
-    var version = bytes.shift();
+    var version = bytes[0];
     if (version != this.PROTOCOL_VERSION) {
       return {error: "protocol version " + version + " doesn't match v2"};
     }
 
-    var deviceId = this.read_int(bytes);
-    var flags = this.read_int(bytes);
+    var deviceId = this.read_int(bytes, 1);
+    var flags = this.read_int(bytes, 3);
     var result = {'Protocol version': version, 'Device ID': deviceId};
     // decode payload
+    var pos = 5;
     for (i = 0; i < this.SENSORS.length; i++, flags >>= 1) {
       if ((flags & 1) !== 1)
         continue;
@@ -75,7 +76,8 @@ var decentlab_decoder = {
       var x = [];
       // convert data to 16-bit integer array
       for (j = 0; j < sensor.length; j++) {
-        x.push(this.read_int(bytes));
+        x.push(this.read_int(bytes, pos));
+        pos += 2;
       }
 
       // decode sensor values
