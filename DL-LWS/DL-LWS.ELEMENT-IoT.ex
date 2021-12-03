6f3f8f3de67ae20f8d53f53ef6589cf7ef103a4e
Khash Jalsan <khash.jalsan@decentlab.com>
2021-12-03 13:57:28 +0100

    DL-LWS: add (pub)

---
+++ DL-LWS/DL-LWS.ELEMENT-IoT.ex
@@ -0,0 +1,45 @@
+defmodule Parser do
+  use Platform.Parsing.Behaviour
+  
+  ## test payloads
+  # 0211110003409a00860c54
+  # 02111100020c54
+  
+  def fields do
+    [
+      %{field: "leaf_wetness_index", display: "Leaf wetness index", unit: ""},
+      %{field: "battery_voltage", display: "Battery voltage", unit: "V"}
+    ]
+  end
+
+  def parse(<<2, device_id::size(16), flags::binary-size(2), words::binary>>, _meta) do
+    {_remaining, result} =
+      {words, %{:device_id => device_id, :protocol_version => 2}}
+      |> sensor0(flags)
+      |> sensor1(flags)
+
+    result
+  end
+
+  
+  defp sensor0({<<x0::size(16), x1::size(16), remaining::binary>>, result},
+               <<_::size(15), 1::size(1), _::size(0)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :leaf_wetness_index => ((x0 + x1*65536) / 8388608 - 1) * 1
+               })}
+  end
+  defp sensor0(result, _flags), do: result
+  
+  defp sensor1({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(14), 1::size(1), _::size(1)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :battery_voltage => x0 / 1000
+               })}
+  end
+  defp sensor1(result, _flags), do: result
+  
+end
---
+++ DL-LWS/DL-LWS.erl
@@ -0,0 +1,89 @@
+-module(decentlab_decoder).
+-define(PROTOCOL_VERSION, 2).
+-export([decode/1, start/0]).
+
+-define(SENSOR_DEFS,
+  [
+    #{
+      length => 2,
+      values => [
+        #{
+          name => <<"Leaf wetness index">>,
+          convert => fun(X) -> ((lists:nth(0 + 1, X) + lists:nth(1 + 1, X)*65536) / 8388608 - 1) * 1 end
+        }
+      ]
+    },
+    #{
+      length => 1,
+      values => [
+        #{
+          name => <<"Battery voltage">>,
+          convert => fun(X) -> lists:nth(0 + 1, X) / 1000 end,
+          unit => <<"V"/utf8>>
+        }
+      ]
+    }
+  ]
+).
+
+
+decode(<<?PROTOCOL_VERSION, DeviceId:16,
+         Flags:16/bitstring, Bytes/binary>> = Binary) when is_binary(Binary) ->
+  Words = bytes_to_words(Bytes),
+  maps:merge(#{<<"Protocol version">> => ?PROTOCOL_VERSION,
+               <<"Device ID">> => DeviceId},
+             sensor(Words, Flags, ?SENSOR_DEFS));
+decode(HexStr) ->
+  Binary = hex2bin(HexStr),
+  decode(Binary).
+
+
+start() ->
+  io:format("~p~n", [decode("0211110003409a00860c54")]),
+  io:format("~p~n", [decode("02111100020c54")]).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIVATE FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sensor(_Words, _Flags, []) -> #{};
+sensor(Words, <<Flags:15, 0:1>>, [_Cur | Rest]) ->
+  sensor(Words, <<0:1, Flags:15>>, Rest);
+sensor(Words, <<Flags:15, 1:1>>,
+       [#{length := Len, values := ValueDefs} | RestDefs]) ->
+  {X, RestWords} = lists:split(Len, Words),
+  Values = value(ValueDefs, X),
+  RestSensors = sensor(RestWords, <<0:1, Flags:15>>, RestDefs),
+  maps:merge(Values, RestSensors).
+
+
+value([], _X) -> #{};
+value([#{convert := Convert,
+         name := Name} = ValueDef | RestDefs], X) ->
+  Values = value(RestDefs, X),
+  Value = #{<<"value">> => Convert(X)},
+  Value2 = add_unit(Value, ValueDef),
+  maps:put(Name, Value2, Values);
+value([_NoConvert | RestDefs], X) -> value(RestDefs, X).
+
+
+add_unit(Value, #{unit := Unit}) ->
+  maps:put(<<"unit">>, Unit, Value);
+add_unit(Value, _NoUnit) ->
+  Value.
+
+
+hex2bin(HexStr) when is_binary(HexStr) -> hex2bin(binary_to_list(HexStr));
+hex2bin(HexStr) -> hex2bin(HexStr, []).
+
+hex2bin("", Bins) -> list_to_binary(lists:reverse(Bins));
+hex2bin(HexStr, Bins) ->
+  Bytes = string:slice(HexStr, 0, 2),
+  Word = list_to_integer(Bytes, 16),
+  hex2bin(string:slice(HexStr, 2), [Word | Bins]).
+
+
+bytes_to_words(Bytes) -> bytes_to_words(Bytes, []).
+
+bytes_to_words(<<>>, Words) -> lists:reverse(Words);
+bytes_to_words(<<Word:16, Rest/binary>>, Words) ->
+  bytes_to_words(Rest, [Word | Words]).
