0c0eddbe76e7b8d22ab45113df732bcde11c440b
Khash Jalsan <khash.jalsan@decentlab.com>
2023-03-02 10:53:34 +0100

    DL-EPAR: add (pub)

---
+++ DL-EPAR/DL-EPAR.ELEMENT-IoT.ex
@@ -0,0 +1,45 @@
+defmodule Parser do
+  use Platform.Parsing.Behaviour
+  
+  ## test payloads
+  # 02445a000381ca0c72
+  # 02445a00020c72
+  
+  def fields do
+    [
+      %{field: "photosynthetically_active_radiation", display: "Photosynthetically active radiation", unit: "µmol⋅m⁻²⋅s⁻¹"},
+      %{field: "battery_voltage", display: "Battery voltage", unit: "V"}
+    ]
+  end
+
+  def parse(<<2, device_id::size(16), flags::binary-size(2), words::binary>>, _meta) do
+    {_remaining, result} =
+      {words, %{:device_id => device_id, :protocol_version => 2}}
+      |> sensor0(flags)
+      |> sensor1(flags)
+
+    result
+  end
+
+  
+  defp sensor0({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(15), 1::size(1), _::size(0)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :photosynthetically_active_radiation => 3 * (x0 / 32768 - 1) / 64 * 1000 * 100
+               })}
+  end
+  defp sensor0(result, _flags), do: result
+  
+  defp sensor1({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(14), 1::size(1), _::size(1)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :battery_voltage => x0 / 1000
+               })}
+  end
+  defp sensor1(result, _flags), do: result
+  
+end
---
+++ DL-EPAR/DL-EPAR.php
@@ -0,0 +1,89 @@
+<?php
+
+abstract class DecentlabDecoder
+{
+    const PROTOCOL_VERSION = 2;
+
+    protected $sensors;
+
+    public function decode($payload = '')
+    {
+        $this->bytes = hex2bin($payload);
+        $this->parts = [];
+
+        $this->parts['version'] = ord($this->bytes[0]);
+        if ($this->parts['version'] != self::PROTOCOL_VERSION) {
+            $this->parts['error'] = sprintf("protocol version %u doesn't match v2", $this->parts['version']);
+            return $this->parts;
+        }
+
+        $this->parts['device_id'] = unpack('n', substr($this->bytes, 1))[1];
+        $flags = unpack('n', substr($this->bytes, 3))[1];
+
+        /* decode payload */
+        $k = 5;
+        foreach ($this->sensors as $sensor) {
+            if (($flags & 1) == 1) {
+                $x = [];
+                /* convert data to 16-bit integer array */
+                for ($j = 0; $j < $sensor['length']; $j++) {
+                    array_push($x, unpack('n', substr($this->bytes, $k))[1]);
+                    $k += 2;
+                }
+
+                /* decode sensor values */
+                foreach ($sensor['values'] as $value) {
+                    if ($value['convert'] != NULL) {
+                        $this->parts[$value['name'] . '_value'] = $value['convert']($x);
+                        if ($value['unit'] != NULL) {
+                            $this->parts[$value['name'] . '_unit'] = $value['unit'];
+                        }
+                    }
+                }
+            }
+            $flags >>= 1;
+        }
+
+        return $this->parts;
+    }
+}
+
+class DL_EPAR_Decoder extends DecentlabDecoder {
+    
+    public function __construct()
+    {
+        $this->sensors = [
+            [
+                'length' => 1,
+                'values' => [
+                    [
+                        'name' => 'photosynthetically_active_radiation',
+                        'convert' => function ($x) { return 3 * ($x[0] / 32768 - 1) / 64 * 1000 * 100; },
+                        'unit' => 'µmol⋅m⁻²⋅s⁻¹',
+                    ],
+                ],
+            ],
+            [
+                'length' => 1,
+                'values' => [
+                    [
+                        'name' => 'battery_voltage',
+                        'convert' => function ($x) { return $x[0] / 1000; },
+                        'unit' => 'V',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
+
+
+$decoder = new DL_EPAR_Decoder();
+$payloads = [
+    '02445a000381ca0c72',
+    '02445a00020c72',
+];
+
+foreach($payloads as $payload) {
+    var_dump($decoder->decode($payload));
+}
