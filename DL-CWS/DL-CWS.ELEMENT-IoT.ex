7bc522a06540453988c3ac3d68981870b2fc210f
Khash Jalsan <khash.jalsan@decentlab.com>
2023-08-22 16:48:36 +0200

    DL-CWS: add (pub)

---
+++ DL-CWS/DL-CWS.ELEMENT-IoT.ex
@@ -0,0 +1,58 @@
+
+# https://www.decentlab.com/support
+
+defmodule Parser do
+  use Platform.Parsing.Behaviour
+  
+  ## test payloads
+  # 02463900038a778a95977a874c80478a5e0b74
+  # 02463900020b74
+  
+  def fields do
+    [
+      %{field: "surface_temperature", display: "Surface temperature", unit: "°C"},
+      %{field: "air_temperature", display: "Air temperature", unit: "°C"},
+      %{field: "air_humidity", display: "Air humidity", unit: "%"},
+      %{field: "dew_point", display: "Dew point", unit: "°C"},
+      %{field: "angle", display: "Angle", unit: "°"},
+      %{field: "sensor_temperature", display: "Sensor temperature", unit: "°C"},
+      %{field: "battery_voltage", display: "Battery voltage", unit: "V"}
+    ]
+  end
+
+  def parse(<<2, device_id::size(16), flags::binary-size(2), words::binary>>, _meta) do
+    {_remaining, result} =
+      {words, %{:device_id => device_id, :protocol_version => 2}}
+      |> sensor0(flags)
+      |> sensor1(flags)
+
+    result
+  end
+
+  
+  defp sensor0({<<x0::size(16), x1::size(16), x2::size(16), x3::size(16), x4::size(16), x5::size(16), remaining::binary>>, result},
+               <<_::size(15), 1::size(1), _::size(0)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :surface_temperature => (x0 - 32768) / 100,
+                 :air_temperature => (x1 - 32768) / 100,
+                 :air_humidity => (x2 - 32768) / 100,
+                 :dew_point => (x3 - 32768) / 100,
+                 :angle => (x4 - 32768),
+                 :sensor_temperature => (x5 - 32768) / 100
+               })}
+  end
+  defp sensor0(result, _flags), do: result
+  
+  defp sensor1({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(14), 1::size(1), _::size(1)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :battery_voltage => x0 / 1000
+               })}
+  end
+  defp sensor1(result, _flags), do: result
+  
+end
---
+++ DL-CWS/DL-CWS.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# https://www.decentlab.com/support
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import struct
+from math import log, floor
+from base64 import binascii
+
+PROTOCOL_VERSION = 2
+
+SENSORS = [
+    {'length': 6,
+     'values': [{'name': 'Surface temperature',
+                 'convert': lambda x: (x[0] - 32768) / 100,
+                 'unit': '°C'},
+                {'name': 'Air temperature',
+                 'convert': lambda x: (x[1] - 32768) / 100,
+                 'unit': '°C'},
+                {'name': 'Air humidity',
+                 'convert': lambda x: (x[2] - 32768) / 100,
+                 'unit': '%'},
+                {'name': 'Dew point',
+                 'convert': lambda x: (x[3] - 32768) / 100,
+                 'unit': '°C'},
+                {'name': 'Angle',
+                 'convert': lambda x: (x[4] - 32768),
+                 'unit': '°'},
+                {'name': 'Sensor temperature',
+                 'convert': lambda x: (x[5] - 32768) / 100,
+                 'unit': '°C'}]},
+    {'length': 1,
+     'values': [{'name': 'Battery voltage',
+                 'convert': lambda x: x[0] / 1000,
+                 'unit': 'V'}]}
+]
+
+
+def decode(msg, hex=False):
+    """msg: payload as one of hex string, list, or bytearray"""
+    bytes_ = bytearray(binascii.a2b_hex(msg)
+                       if hex
+                       else msg)
+    version = bytes_[0]
+    if version != PROTOCOL_VERSION:
+        raise ValueError("protocol version {} doesn't match v2".format(version))
+
+    devid = struct.unpack('>H', bytes_[1:3])[0]
+    bin_flags = bin(struct.unpack('>H', bytes_[3:5])[0])
+    flags = bin_flags[2:].zfill(struct.calcsize('>H') * 8)[::-1]
+
+    words = [struct.unpack('>H', bytes_[i:i + 2])[0]
+             for i
+             in range(5, len(bytes_), 2)]
+
+    cur = 0
+    result = {'Device ID': devid, 'Protocol version': version}
+    for flag, sensor in zip(flags, SENSORS):
+        if flag != '1':
+            continue
+
+        x = words[cur:cur + sensor["length"]]
+        cur += sensor["length"]
+        for value in sensor['values']:
+            if 'convert' not in value:
+                continue
+
+            result[value['name']] = {'value': value['convert'](x),
+                                     'unit': value.get('unit', None)}
+
+    return result
+
+
+if __name__ == '__main__':
+
+    import pprint
+    payloads = [
+        b'02463900038a778a95977a874c80478a5e0b74',
+        b'02463900020b74',
+    ]
+    for pl in payloads:
+        pprint.pprint(decode(pl, hex=True))
+        print("")
