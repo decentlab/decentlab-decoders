63b3c7fd22e9bd35cd12c7c35c54ac04252f63a0
Decentlab <17007399+decentlab@users.noreply.github.com>
2024-10-21 15:30:22 +0200

    DL-IFD: add (pub)

---
+++ DL-IFD/DL-IFD.php
@@ -0,0 +1,90 @@
+<?php
+/* https://www.decentlab.com/fruit-dendrometer-sensor-for-lorawan */
+
+abstract class DecentlabDecoder
+{
+    const PROTOCOL_VERSION = 2;
+
+    protected $sensors;
+
+    public function decode($payload = '')
+    {
+        $bytes = hex2bin($payload);
+        $parts = [];
+
+        $parts['version'] = ord($bytes[0]);
+        if ($parts['version'] != self::PROTOCOL_VERSION) {
+            $parts['error'] = sprintf("protocol version %u doesn't match v2", $parts['version']);
+            return $parts;
+        }
+
+        $parts['device_id'] = unpack('n', substr($bytes, 1))[1];
+        $flags = unpack('n', substr($bytes, 3))[1];
+
+        /* decode payload */
+        $k = 5;
+        foreach ($this->sensors as $sensor) {
+            if (($flags & 1) == 1) {
+                $x = [];
+                /* convert data to 16-bit integer array */
+                for ($j = 0; $j < $sensor['length']; $j++) {
+                    array_push($x, unpack('n', substr($bytes, $k))[1]);
+                    $k += 2;
+                }
+
+                /* decode sensor values */
+                foreach ($sensor['values'] as $value) {
+                    if ($value['convert'] != NULL) {
+                        $parts[$value['name'] . '_value'] = $value['convert']($x);
+                        if ($value['unit'] != NULL) {
+                            $parts[$value['name'] . '_unit'] = $value['unit'];
+                        }
+                    }
+                }
+            }
+            $flags >>= 1;
+        }
+
+        return $parts;
+    }
+}
+
+class DL_IFD_Decoder extends DecentlabDecoder {
+    
+    public function __construct()
+    {
+        $this->sensors = [
+            [
+                'length' => 1,
+                'values' => [
+                    [
+                        'name' => 'fruit_size',
+                        'convert' => function ($x) { return $x[0] / 100; },
+                        'unit' => 'mm',
+                    ],
+                ],
+            ],
+            [
+                'length' => 1,
+                'values' => [
+                    [
+                        'name' => 'battery_voltage',
+                        'convert' => function ($x) { return $x[0] / 1000; },
+                        'unit' => 'V',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
+
+
+$decoder = new DL_IFD_Decoder();
+$payloads = [
+    '0256dc000302bc0c48',
+    '0256dc00020c48',
+];
+
+foreach($payloads as $payload) {
+    var_dump($decoder->decode($payload));
+}
