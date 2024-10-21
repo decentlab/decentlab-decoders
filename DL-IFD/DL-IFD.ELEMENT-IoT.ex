5fd6e6bf5c1d2171e5158353764da2cc4613c967
Decentlab <17007399+decentlab@users.noreply.github.com>
2024-10-21 15:30:22 +0200

    DL-IFD: add (pub)

---
+++ DL-IFD/DL-IFD.ELEMENT-IoT.ex
@@ -0,0 +1,51 @@
+
+# https://www.decentlab.com/fruit-dendrometer-sensor-for-lorawan
+
+defmodule Parser do
+  use Platform.Parsing.Behaviour
+  
+  ## test payloads
+  # 0256dc000302bc0c48
+  # 0256dc00020c48
+  
+  def fields do
+    [
+      %{field: "fruit_size", display: "Fruit size", unit: "mm"},
+      %{field: "battery_voltage", display: "Battery voltage", unit: "V"}
+    ]
+  end
+
+  def parse(<<2, device_id::size(16), flags::binary-size(2), words::binary>>, _meta) do
+    {_remaining, result} =
+      {words, %{:device_id => device_id, :protocol_version => 2}}
+      |> sensor0(flags)
+      |> sensor1(flags)
+
+    result
+  end
+
+  defp where(true, if_true, _if_false), do: if_true
+  defp where(false, _if_true, if_false), do: if_false
+
+  
+  defp sensor0({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(15), 1::size(1), _::size(0)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :fruit_size => x0 / 100
+               })}
+  end
+  defp sensor0(result, _flags), do: result
+  
+  defp sensor1({<<x0::size(16), remaining::binary>>, result},
+               <<_::size(14), 1::size(1), _::size(1)>>) do
+    {remaining,
+     Map.merge(result,
+               %{
+                 :battery_voltage => x0 / 1000
+               })}
+  end
+  defp sensor1(result, _flags), do: result
+  
+end
